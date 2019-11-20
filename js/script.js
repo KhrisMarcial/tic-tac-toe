1a58dd02389a9656a20a78015e9d865a49754fe8
Khris <khristianmarcial@gmail.com>
2019-11-19 21:50:44 -0500

    added constants

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//IPO
+//Input Process Output
+
+//1. Define Inputs - Constants & State Variables
+
+const COMBOS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [2,4,6],
+    [0,4,8],
+    [1,4,7],
+    [3,4,5],
+    [2,5,8],
+    [0,3,6],
+];
+
+const KEY = {
+    '1':'X',
+    '-1':'O'
+};
+
